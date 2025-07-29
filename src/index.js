9a78662da97c24d54cf984175ce6dc0897991501
Razvan Neculaes <razvan.neculaes@osf.digital>
2025-07-29 15:56:04 +0300

    Update the texts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // src/index.js
 import 'typeface-ubuntu';
+import 'typeface-bebas-neue';
 import './style.scss'; // Import your main Sass file
 
 // You can add your JavaScript code here or import other JS modules
