7360a0dfce733b6c47e10ceaeaeb3ff381d2f68b
Razvan Neculaes <razvan.neculaes@osf.digital>
2025-08-04 17:03:44 +0300

    NEwsletter section improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-// webpack.config.js
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // <-- Add this line
 
 module.exports = {
     // Set the mode: 'development' for dev server, 'production' for optimized build
@@ -24,11 +24,9 @@ module.exports = {
             {
                 test: /\.s[ac]ss$/i, // Matches .scss and .sass files
                 use: [
-                    // In development, use 'style-loader' to inject CSS into the DOM.
-                    // In production, use 'MiniCssExtractPlugin.loader' to extract CSS into a separate file.
-                    MiniCssExtractPlugin.loader, // Use this for both dev and prod for external CSS
-                    'css-loader', // Interprets @import and url()
-                    'sass-loader', // Compiles Sass to CSS
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
                 ],
             },
             // Rule for regular CSS files
@@ -39,29 +37,25 @@ module.exports = {
                     'css-loader',
                 ],
             },
-            // Rule for images (optional, but good practice if you have any in CSS)
+            // Rule for images (handled by Webpack 5)
+            // Note: This rule is good, but when using HtmlWebpackPlugin,
+            // the path in HTML isn't processed by this loader automatically.
+            // CopyWebpackPlugin is a more direct fix for this specific issue.
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource', // Webpack 5 way to handle assets
+                type: 'asset/resource',
+                generator: {
+                    filename: 'img/[name][ext]' // Ensure images are in an 'img' folder in the output
+                }
             },
             // Rule for handling font files
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
                 generator: {
-                    // This makes sure the output path for fonts is correct
-                    // For example, they'll go into dist/fonts/
                     filename: 'fonts/[name][ext]'
                 }
             },
-            // You can add rules for JavaScript here later if needed
-            // {
-            //     test: /\.js$/,
-            //     exclude: /node_modules/,
-            //     use: {
-            //         loader: 'babel-loader', // If you use Babel for modern JS features
-            //     },
-            // },
         ],
     },
 
@@ -71,21 +65,28 @@ module.exports = {
             filename: 'style.css', // Name of the extracted CSS file
         }),
         new HtmlWebpackPlugin({
-            template: './src/index.html', // Path to your source HTML file
-            filename: 'index.html', // Output HTML file name in 'dist'
-            // You can add title, favicon etc. here
-            // title: 'One Step Website',
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+        // Add CopyWebpackPlugin here to copy your assets
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: 'src/img', // Source directory to copy from
+                    to: 'img', // Destination directory relative to the output path
+                },
+            ],
         }),
     ],
 
-    // Development server configuration (optional, but very useful)
+    // Development server configuration
     devServer: {
         static: {
             directory: path.join(__dirname, 'dist'),
         },
         compress: true,
-        port: 8080, // You can change the port
-        open: true, // Open the browser automatically
-        hot: true, // Enable Hot Module Replacement
+        port: 8080,
+        open: true,
+        hot: true,
     },
 };
