1a78f2bf0440f7eab722d8f51413d8312af96fcf
Razvan Neculaes <razvan.neculaes@osf.digital>
2025-08-22 16:48:54 +0300

    Reset

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,9 +1,1 @@
-document.querySelectorAll('.footer-col').forEach((col) => {
-    const button = col.querySelector('.toggle-button');
-    const content = col.querySelector('.collapsible-content');
-
-    button.addEventListener('click', () => {
-        const isActive = col.classList.toggle('active');
-        button.textContent = isActive ? '-' : '+';
-    });
-});
+console.log('load the footer...');
